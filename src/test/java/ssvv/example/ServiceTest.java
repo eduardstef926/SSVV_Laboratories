686e4cb4a0e376bff4a98b75d94f1f84303549fa
EdiStef <eddistef@gmail.com>
2023-05-12 12:09:30 +0300

    AddedIncrementalIntegrationOfTests

--- src/test/java/ssvv/example/ServiceTest.java
+++ src/test/java/ssvv/example/ServiceTest.java
@@ -37,56 +37,51 @@ public class ServiceTest {
     }
 
     @Test
-    public void testAddStudent() {
+    public void testAddStudentBigBang() {
         Student student = new Student("1", "John Doe", 934);
         when(studentRepo.save(student)).thenReturn(student);
-
         service.saveStudent("1", "John Doe", 934);
-
         verify(studentRepo, times(1)).save(student);
     }
 
     @Test
-    public void testAddAssignment() {
+    public void testAddAssignmentBigBang() {
         Tema tema = new Tema("1", "Assignment 1", 2, 1);
-
         when(temaRepo.findOne("1")).thenReturn(null);
         when(temaRepo.save(tema)).thenReturn(tema);
-
         assertEquals(0, service.saveTema("1", "Assignment 1", 2, 1));
     }
 
     @Test
-    public void testAddGrade() {
+    public void testAddGradeBigBang() {
         Student student = new Student("1", "John Doe", 934);
         Tema tema = new Tema("1", "Assignment 1", 2, 1);
         Pair<String, String> idNota = new Pair<>("1", "1");
         Nota nota = new Nota(idNota, 9.5, 2, "feedback");
-
         when(studentRepo.findOne("1")).thenReturn(student);
         when(temaRepo.findOne("1")).thenReturn(tema);
         when(notaRepo.save(nota)).thenReturn(nota);
-
         assertEquals(1, service.saveNota("1", "1", 9.5, 2, "feedback"));
     }
 
     @Test
     public void testIntegration() {
-        testAddStudent();
-
-        testAddAssignment();
-
-        testAddGrade();
+        testAddStudentBigBang();
+        testAddAssignmentBigBang();
+        testAddGradeBigBang();
     }
 
 
     @Test
-    public void testAddStudentAndAssignment() {
+    public void testAddStudentIncremental() {
         Student student = new Student("2", "Sylvester Stallone", 936);
         when(studentRepo.save(student)).thenReturn(student);
         service.saveStudent("2", "Sylvester Stallone", 936);
         verify(studentRepo, times(1)).save(student);
+    }
 
+    @Test
+    public void testAddAssignmentIncremental() {
         Tema tema = new Tema("2", "Assignment 2", 5, 2);
         when(temaRepo.findOne("2")).thenReturn(null);
         when(temaRepo.save(tema)).thenReturn(tema);
@@ -94,24 +89,15 @@ public class ServiceTest {
     }
 
     @Test
-    public void testAddStudentAssignmentAndGrade() {
+    public void testAddGradeIncremental() {
         Student student = new Student("2", "Sylvester Stallone", 936);
-        when(studentRepo.save(student)).thenReturn(student);
-        service.saveStudent("2", "Sylvester Stallone", 936);
-        verify(studentRepo, times(1)).save(student);
-
+        when(studentRepo.findOne("2")).thenReturn(student);
         Tema tema = new Tema("2", "Assignment 2", 5, 2);
-        when(temaRepo.findOne("2")).thenReturn(null);
-        when(temaRepo.save(tema)).thenReturn(tema);
-        assertEquals(0, service.saveTema("2", "Assignment 2", 5, 2));
-
+        when(temaRepo.findOne("2")).thenReturn(tema);
         Pair<String, String> idNota = new Pair<>("2", "2");
         Nota nota = new Nota(idNota, 10, 4, "Good Job");
-
-        when(studentRepo.findOne("2")).thenReturn(student);
-        when(temaRepo.findOne("2")).thenReturn(tema);
         when(notaRepo.save(nota)).thenReturn(nota);
-
         assertEquals(1, service.saveNota("2", "2", 10, 4, "Good Job"));
     }
+
 }
